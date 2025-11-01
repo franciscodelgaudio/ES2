529c86bb68549777dc252053d1aad2470b6c32ba
franciscodelgaudio <delgaudiofrancisco.junior@gmail.com>
2025-11-01 18:16:45 -0300

    Abrir portas da api para o vercel

--- backend/src/main/java/com/es2/equipe4/controller/EventController.java
+++ backend/src/main/java/com/es2/equipe4/controller/EventController.java
@@ -11,7 +11,6 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/api")
 public class EventController {
---
+++ backend/src/main/java/com/es2/equipe4/controller/RootController.java
@@ -0,0 +1,13 @@
+package com.es2.equipe4.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class RootController {
+
+    @GetMapping("/")
+    public String root() {
+        return "OK";
+    }
+}
